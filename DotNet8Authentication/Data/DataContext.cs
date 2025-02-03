fc8745aca6214f21a8576567433ee17e186d8376
Home <Home@DESKTOP-DP2AA4V>
2025-02-04 00:54:31 +0400

    Добавьте файлы проекта.

---
+++ DotNet8Authentication/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8Authentication.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options) 
+        {
+            
+        }
+    }
+
+    
+}
